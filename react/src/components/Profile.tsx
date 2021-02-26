7955e6e92369c17c5fecdd458893243533fb21b0
Ednan Dias <ednan.741@gmail.com>
2021-02-26 09:43:56 -0300

    adicionando contextos

--- react/src/components/Profile.tsx
+++ react/src/components/Profile.tsx
@@ -1,16 +1,19 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/ednan-dias.png" alt="Ednan Dias" />
-            <div>
-                <strong> Ednan Dias </strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level 1
-                    </p>
-            </div>
-        </div>
-    )
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/ednan-dias.png" alt="Ednan Dias" />
+      <div>
+        <strong> Ednan Dias </strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
